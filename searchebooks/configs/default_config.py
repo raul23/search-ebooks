f4cd601ed2bb41966f5887f323176fe171d2f707
Raul <rchfe23@gmail.com>
2021-06-11 01:44:54 -0400

    default_config: add edit options

--- searchebooks/configs/default_config.py
+++ searchebooks/configs/default_config.py
@@ -4,6 +4,7 @@ import os
 # ==================
 # 1. General options
 # ==================
+input_data = None
 
 # 1.1 General control flags
 # =========================
@@ -12,23 +13,24 @@ verbose = False
 regex = False
 ignore_case = False
 
-# 1.2 Options for OCR
-# ===================
-ocr_enabled = 'false'
-ocr_only_first_last_pages = (7, 3)
-ocr_command = 'tesseract_wrapper'
-
-# 1.3 Miscellaneous options
+# 1.2 Miscellaneous options
 # =========================
+ocr_enabled = 'false'
 logging_level = 'info'
 logging_formatter = 'only_msg'
-# Reverse sort
-reverse = False
+
+# ===============
+# 2. Edit options
+# ===============
+edit = False
+app = None
+reset = False
+cfg = 'main'
 
 # ================
-# 2. Cache options
+# 3. Cache options
 # ================
-use_cache = False
+use_cache = True
 cache_folder = os.path.expanduser('~/.searchebooks')
 eviction_policy = 'least-recently-stored'
 # In gigabytes (GB)
@@ -36,9 +38,10 @@ cache_size_limit = 1
 clear_cache = False
 
 # ==============================
-# 3. Text-content search options
+# 4. Text content search options
 # ==============================
-search_query = None
+# Use raw string (r'string') for search query to be able to use regex anchors like \b
+query = r''
 ebook_formats = ['djvu', 'epub', 'html', 'pdf', 'txt']
 djvu_search_method = 'djvutxt'
 msword_search_method = 'textutil'
@@ -48,8 +51,9 @@ text_regex = False
 text_ignore_case = False
 
 # ==========================
-# 4. Metadata search options
+# 5. Metadata search options
 # ==========================
+# Use raw string (r'string') for metadata to be able to use regex anchors like \b
 authors = None
 book_producer = None
 category = None
